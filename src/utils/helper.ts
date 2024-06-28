be8b6f5855c52c8880cef01a500b1de2501bf19a
Babatunde Ojo <babatundeojo30@gmail.com>
2024-06-28 16:37:10 +0100

    add helper and validator file

---
+++ src/utils/helper.ts
@@ -0,0 +1,86 @@
+export const getCompetitions = () => {
+    return [
+        { label: "Sports", value: "Sports" },
+        { label: "Cultural and Artistic", value: "Cultural and Artistic" },
+        { label: "Academy", value: "Academy" },
+        { label: "Technology and Innovation", value: "Technology and Innovation" },
+        { label: "Gaming and eSports", value: "Gaming and eSports" },
+        { label: "Beauty Pageants and Fashion", value: "Beauty Pageants and Fashion" },
+        { label: "Culinary", value: "Culinary" },
+    ];;
+}
+
+export const getHost = () => {
+    return [
+        {
+            id: "1",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "2",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "3",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "4",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "5",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "6",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "7",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "8",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "9",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        },
+        {
+            id: "10",
+            title: "20th Asian Game - Achi Nagoya 2026 (Winter)",
+            startDate: "2024-01-01",
+            endDate: "2024-09-01",
+            description: "Pyeongchang, Gangwon-do, Korea Very Very long city name name name name" 
+        }
+    ]
+}
