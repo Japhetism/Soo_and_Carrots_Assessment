2aa6a19f7658f55cedfb40bac864277851125be3
Babatunde Ojo <babatundeojo30@gmail.com>
2024-06-28 16:41:43 +0100

    add app navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import SplashScreen from "./src/screens/splash";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen  from "./src/screens/splash";
+import AccountCreationScreen from "./src/screens/signup/account_creation";
+import CompetitionScreen from "./src/screens/signup/competition";
+import { navigationRef } from "./src/services/Navigation/NavigationService";
+
+const Stack = createStackNavigator();
 
 const App = () => {
     return (
-        <SplashScreen />
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator 
+                initialRouteName="Home"
+                screenOptions={{ headerShown: false }}
+            >
+                <Stack.Screen name="Home" component={SplashScreen} />
+                <Stack.Screen name="Signup" component={AccountCreationScreen} />
+                <Stack.Screen name="Competition" component={CompetitionScreen} />
+            </Stack.Navigator>
+      </NavigationContainer>
     );
 };
 
-export default App
+export default App;
+
 // export {default} from './.storybook'; 
